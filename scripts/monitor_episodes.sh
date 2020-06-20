737b1780b4c091b045f52b7e6efe2009037b19ed
Blair Watkinson <blair.watkinson@me.com>
2020-06-20 23:37:59 +0000

    monitor offense on ball tests

--- scripts/monitor_episodes.sh
+++ scripts/monitor_episodes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-folders="empty_goal_000 empty_goal_001 empty_goal_002 empty_goal_003 empty_goal_004 empty_goal_005 empty_goal_006 empty_goal_007 defended_goal_000"
+folders="empty_goal_000 empty_goal_001 empty_goal_002 empty_goal_003 empty_goal_004 empty_goal_005 empty_goal_006 empty_goal_007 defended_goal_000 defended_goal_001 test_defended_goal_on_ball_000 test_defended_goal_on_ball_001"
 
 for f in ${folders}
 do
